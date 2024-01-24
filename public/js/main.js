b4cd09475a8db686dd9c122fb6e880613a59a108
Laetitia Gallegos Lopez <laetitiagallegoslopez@MacBook-Air-de-Laetitia.local>
2024-01-24 16:57:49 +0100

    Layout

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
     let newArray = []
 
-    for (let i = 0; i < randomImage.length; i++) {
-        let j = Math.floor(Math.random()* randomImage.length)
-        newArray.push(randomImage[j]) 
+    while (randomImage.length > 0) {
+        let i = Math.floor(Math.random() * randomImage.length);
+        newArray.push(randomImage[i]);
+        randomImage.splice(i, 1);
     }
 
     for (let index = 0; index < cards.length; index++) {
@@ -28,37 +29,38 @@ document.addEventListener('DOMContentLoaded', () => {
 let allcard = document.querySelectorAll(".soloCard")
 
 let arrayDobble = [] //array that will take the 2 cards and compare them 
- 
+
+//Every time we click on a card:
 allcard.forEach(element => {
     element.addEventListener('click', (e) =>{
-        element.classList.remove("back") //retirer la classe back(face cachée)
-        element.classList.add("front") //ajouter la classe front(face visible)
-        arrayDobble.push(element.firstElementChild.getAttribute("src")) 
-        element.firstChild.style.display="flex"
+        element.classList.remove("back") //remove the class "back" (hidden face)
+        element.classList.add("front") //add the class "front" (visible face)
+        arrayDobble.push(element.firstElementChild.getAttribute("src"))  //push the src of the div car into the array : will be compare on the condition below (when 2 cards are added)
+        element.firstChild.style.display="flex" //display the image (from "none" to "flex")
 
-        let frontcarte = document.querySelectorAll(".front")[0]
-        let frontcarteDeux = document.querySelectorAll(".front")[1]
+        let frontcarte = document.querySelectorAll(".front")[0] //1st card
+        let frontcarteDeux = document.querySelectorAll(".front")[1] //2st card
 
-        if (arrayDobble.length == 2) {
-            if (arrayDobble[0] == arrayDobble[1]) {
+        if (arrayDobble.length == 2) { //when the array is filled with 2 cards
+            if (arrayDobble[0] == arrayDobble[1]) { //if the 2 cards are the same
                 frontcarte.classList.add("disabledCard")
                 frontcarteDeux.classList.add("disabledCard")
                 frontcarte.classList.add("correct")
                 frontcarte.classList.remove("front")
                 frontcarteDeux.classList.add("correct")
                 frontcarteDeux.classList.remove("front")
-                arrayDobble = [] //vider le tableau
+                arrayDobble = [] //empty out the array for the next click
 
             } else {
-                setTimeout(() => { //mettre un délai si les deux cartes sont différentes car en un click (addevent) il y a plusieurs actions qui se réalisent en même temps (voir au-dessus)
+                setTimeout(() => { //mettre un délai si les deux cartes sont différentes car en un click (addevent) il y a plusieurs actions qui se réalisent en même temps (voir au-dessus) // adding a delay if the two cards are differents
                     frontcarte.classList.remove("front")
                     frontcarte.classList.add("back")
                     frontcarteDeux.classList.remove("front")
                     frontcarteDeux.classList.add("back")
                     frontcarte.firstChild.style.display="none"
                     frontcarteDeux.firstChild.style.display="none"
-                    arrayDobble = []
-                }, 800);
+                    arrayDobble = [] //empty out the array for the next click
+                }, 1000);
             }
         }   
     }) 
